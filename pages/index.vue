a28b12948c40cf3465d89d90c967752c78674f7e
JHerrera458 <julianherrera33@gmail.com>
2023-03-29 09:33:45 -0500

    Se realiza el get de los heroes, se muestran en una lista

--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,9 @@
     </v-row>
 
     <v-row>
-      <v-col cols="3" align="center">
-        <v-card>
-          <v-img height="300" src="http://i.annihil.us/u/prod/marvel/i/mg/2/80/4c002f35c5215.jpg"></v-img>
-          <v-card-title primary-title>
-            Ajak
-          </v-card-title>
-        </v-card>
-      </v-col>
+      <li v-for="hero in heroes.results">
+        {{ hero.name }}
+      </li>
     </v-row>
   </v-container>
 </template>
@@ -22,8 +17,27 @@
 <script>
 export default {
   layout: "blank",
+  beforeMount(){
+    this.loadHeroes()
+  },
   data() {
     return {
+      myHero: {
+        name: "",
+        image: "",
+      },
+      heroes:[],
+    }
+  },
+  methods: {
+    loadHeroes() {
+      const url = "https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=0&ts=1&apikey=21a4da889edbe77bc6cb0a69e352ec87&hash=84fbb30897280ab8b56fcdd59fb4ffe2"
+      this.$axios.get(url).then(response => {
+        this.heroes = response.data.data
+        console.log(this.heroes);
+      }).catch(error => {
+        console.log(error);
+      })
     }
   }
 }
